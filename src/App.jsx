e544eb64c882a432c6d708fee074f34ce9542118
unknown <habiburrahmanofficial157@gmail.com>
2023-09-18 12:00:18 +0600

    lineChart and phone section code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
-import DaisyNav from "./components/DaisyNav/DaisyNav";
+import LineChart from "./components/LineChart/LineChart";
+// import DaisyNav from "./components/DaisyNav/DaisyNav";
 import Navber from "./components/Navber/Navber";
+import Phones from "./components/Phones/Phones";
+import PriceOptions from "./components/PriceOptions/PriceOptions";
 
 function App() {
   return (
     <>
-      <DaisyNav></DaisyNav>
       <Navber></Navber>
+      {/* <DaisyNav></DaisyNav> */}
       <h1 className="text-7xl bg-rose-600">Vite + React</h1>
+      <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
 const Link = ({ route }) => {
   return (
-    <li className="mr-10">
+    <li className="mr-10 px-6 hover:bg-yellow-500">
       <a href={route.path}>{route.name}</a>
     </li>
   );
 };
-Link.protoTypes={
-
-}
+Link.protoTypes = {
+  route: PropTypes.object,
+};
 export default Link;
